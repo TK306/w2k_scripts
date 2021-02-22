1f9486080546bb76e7311339d0c64be6948ba7ff
TK306 <per.tk306@gmail.com>
2021-02-22 17:21:02 +0900

    [fix] Reformat

--- conv_check.py
+++ conv_check.py
@@ -21,36 +21,36 @@ v_start = 5
 v_step = 0.5
 v_end = 10
 for v in np.arange(v_start, v_end + v_step / 2, v_step):
-  if not os.path.exists(w2k.case_path + 'stop.txt'):
-    w2k.rkmax = v
-    w2k.lmax = 10
-    w2k.kmesh = 10000
-    w2k.gmax = 12
+	if not os.path.exists(w2k.case_path + 'stop.txt'):
+		w2k.rkmax = v
+		w2k.lmax = 10
+		w2k.kmesh = 10000
+		w2k.gmax = 12
 
-    sp.run('rm *.scf*', shell=True)
-    sp.run('rm *.broyd*', shell=True)
-    w2k.init_lapw()
-    dt_s = dt.datetime.now()
-    w2k.run_scf()
-    scf_time = dt.datetime.now() - dt_s
-    etot = w2k.get_etot()
-    etot_ls.append(etot)
-    scf_time_ls.append(scf_time.seconds)
+		sp.run('rm *.scf*', shell=True)
+		sp.run('rm *.broyd*', shell=True)
+		w2k.init_lapw()
+		dt_s = dt.datetime.now()
+		w2k.run_scf()
+		scf_time = dt.datetime.now() - dt_s
+		etot = w2k.get_etot()
+		etot_ls.append(etot)
+		scf_time_ls.append(scf_time.seconds)
 
-    vstr = str(v).replace('.', 'p')
-    dosname = 'dos' + vstr
-    scfname = 'scf' + vstr
-    w2k.run_dos(dosout, dosname)
+		vstr = str(v).replace('.', 'p')
+		dosname = 'dos' + vstr
+		scfname = 'scf' + vstr
+		w2k.run_dos(dosout, dosname)
 
-    sp.run(['cp', w2k.filepath('.scf'), scfout + scfname + '.scf'])
+		sp.run(['cp', w2k.filepath('.scf'), scfout + scfname + '.scf'])
 
-    etotw = iw.IgorWave(np.array(etot_ls), name='etot')
-    scftw = iw.IgorWave(np.array(scf_time_ls), name='scftime')
-    etotw.set_dimscale('x', v_start, v_step, '')
-    scftw.set_dimscale('x', v_start, v_step, '')
-    etotw.set_datascale('eV')
-    scftw.set_datascale('sec')
-    with open(convdir + 'scflog.itx', 'w') as f:
-      etotw.save_itx(f)
-      scftw.save_itx(f)
-    an.make_dos_waves([dosout])
+		etotw = iw.IgorWave(np.array(etot_ls), name='etot')
+		scftw = iw.IgorWave(np.array(scf_time_ls), name='scftime')
+		etotw.set_dimscale('x', v_start, v_step, '')
+		scftw.set_dimscale('x', v_start, v_step, '')
+		etotw.set_datascale('eV')
+		scftw.set_datascale('sec')
+		with open(convdir + 'scflog.itx', 'w') as f:
+			etotw.save_itx(f)
+			scftw.save_itx(f)
+		an.make_dos_waves([dosout])
