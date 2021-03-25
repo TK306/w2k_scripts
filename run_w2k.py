b578da0ec2aa520188198f150df7820c0371a497
T. Kono <59307945+TK306@users.noreply.github.com>
2021-03-25 12:36:36 +0900

    Update run_w2k.py
    バグ修正

--- run_w2k.py
+++ run_w2k.py
@@ -256,7 +256,7 @@ class W2k:
 				path = self.filepath('.dos' + str(n) + 'eV' + spin)
 				savepath = outfol + name + '.dos' + str(n) + 'eV' + spin
 				if os.path.exists(path):
-					subprocess.call(["", path, savepath])
+					subprocess.call(["cp", path, savepath])
 				else:
 					break
 				n += 1
